e4e4ce7ee63f585109f6879a9d4df232fca61188
IkramMahfoud <ikrammahfoud9nemo@gmail.com>
2023-12-20 00:55:43 +0100

    DIG-70: Création du Component 404 si lURL est invalide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './components/LoginForm'
 import Apartement from './components/Appartement';
 import AddAppartement from './components/AddAppartement'
 import UpdateAppartement from './components/UpdateAppartement'
+import NotFound from './components/NotFound.jsx'
 
 
 
@@ -22,6 +23,7 @@ function App()
             {/* Nested Routes for Apartement */}
             <Route exact path="/apartement/add" component={AddAppartement} />
             <Route exact path="/apartement/update/:apartmentId/:etage/:number/:owner" component={UpdateAppartement} />
+            <Route component={NotFound} />
 
           </Switch>
         </BrowserRouter>
