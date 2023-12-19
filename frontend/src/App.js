416ade11917db3c2afe64062df20039ad75f233b
IkramMahfoud <ikrammahfoud9nemo@gmail.com>
2023-12-20 00:33:37 +0100

    DIG-17: Définir la structure des routes en utilisant NestedRoutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from './components/LoginForm'
-import  Apartement from './components/Appartement';
+import Apartement from './components/Appartement';
+import AddAppartement from './components/AddAppartement'
+import UpdateAppartement from './components/UpdateAppartement'
 
 
 
@@ -11,19 +13,17 @@ function App()
 
       <>
         <BrowserRouter>
-
           <Switch>
 
             <Route exact path="/">
               <LoginPage />
             </Route>
+            <Route exact path="/apartement" component={Apartement} />
+            {/* Nested Routes for Apartement */}
+            <Route exact path="/apartement/add" component={AddAppartement} />
+            <Route exact path="/apartement/update/:apartmentId/:etage/:number/:owner" component={UpdateAppartement} />
 
-            <Route exact path="/apartement">
-              <Apartement/>
-            </Route>
           </Switch>
-
-
         </BrowserRouter>
       </>
 
